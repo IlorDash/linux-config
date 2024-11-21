919ffadeae0a36ce855fc9d71d99032b0d43b363
Ilya Orazov <i.orazov@akytec.rs>
2024-11-21 12:25:39 +0300

    Add btop, store package versions in env var, run share-clipbrd at startup
    Also fix vscode link to actual latest deb.

--- my-config.sh
+++ my-config.sh
@@ -2,6 +2,17 @@
 
 set -ex
 
+# Packages versions
+
+SHARE_CLIPBRD_VER="1.0.3"
+GCM_VER="2.6.0"
+BTOP_VER="1.4.0"
+
+# User
+
+GIT_USERNAME="IlorDash"
+GIT_EMAIL="ilordash02@gmail.com"
+
 # Setup Full HD resolution
 
 if sudo grep -q '^GRUB_CMDLINE_LINUX_DEFAULT=' /etc/default/grub; then
@@ -29,24 +40,44 @@ sudo apt-get -y install speedtest-cli
 
 # Share Clipboard
 
-wget https://github.com/viordash/ShareClipbrd/releases/download/v1.0.3/ShareClipbrd-linux64.tar
+wget https://github.com/viordash/ShareClipbrd/releases/download/v${SHARE_CLIPBRD_VER}/ShareClipbrd-linux64.tar
 mkdir ShareClipbrdApp
 tar -xvf ShareClipbrd-linux64.tar -C ShareClipbrdApp
 sudo ln -s $(pwd)/ShareClipbrdApp/ShareClipbrdApp /usr/bin/share-clipbrd
+# Run Share Clipboard at startup
+touch /home/${USER}/.config/autostart/share-clipbrd.desktop
+cat > /home/${USER}/.config/autostart/share-clipbrd.desktop << EOL
+[Desktop Entry]
+Type=Application
+Exec=share-clipbrd
+Hidden=false
+Name[en_US]=share-clipbrd
+Name=share-clipbrd
+Comment[en_US]=
+Comment=
+X-MATE-Autostart-Delay=0
+EOL
 
 # VS Code
 
-wget https://go.microsoft.com/fwlink/?LinkID=760868 -O vscode.deb
+wget https://update.code.visualstudio.com/latest/linux-deb-x64/stable -O vscode.deb
 sudo apt -y install ./vscode.deb
 
 # Git
 
-git config --global user.name "IlorDash"
-git config --global user.email "ilordash02@gmail.com"
+git config --global user.name "${GIT_USERNAME}"
+git config --global user.email "${GIT_EMAIL}"
 git config --global credential.credentialStore cache
-wget https://github.com/git-ecosystem/git-credential-manager/releases/download/v2.6.0/gcm-linux_amd64.2.6.0.tar.gz -O gcm-linux_amd64.2.6.0.tar.gz
-sudo tar -xvf gcm-linux_amd64.2.6.0.tar.gz -C /usr/local/bin
+wget https://github.com/git-ecosystem/git-credential-manager/releases/download/v$GCM_VERSION/gcm-linux_amd64.${GCM_VERSION}.tar.gz -O gcm-linux_amd64.${GCM_VERSION}.tar.gz
+sudo tar -xvf gcm-linux_amd64.${GCM_VERSION}.tar.gz -C /usr/local/bin
 git-credential-manager configure
+
+# btop
+
+wget https://github.com/aristocratos/btop/releases/download/v${BTOP_VER}/btop-x86_64-linux-musl.tbz
+cd btop-x86_64-linux-musl.tbz
+sudo make install
+sudo make setcap
 
 sudo apt autoremove
 sudo reboot now
