fd3802df331abb63cefeb5ad97fb97eff0e2752a
IlorDash <ilordash02@gmail.com>
2025-01-21 18:09:28 +0100

    config: Fetch latest stable i3 from sur5r, install i3lock-color

--- my-config.sh
+++ my-config.sh
@@ -77,10 +77,28 @@ sudo make setcap
 
 # i3wm
 
-sudo apt -y install xorg lightdm lightdm-gtk-greeter i3-wm i3lock i3status i3blocks dmenu \
+/usr/lib/apt/apt-helper download-file \
+    https://debian.sur5r.net/i3/pool/main/s/sur5r-keyring/sur5r-keyring_2024.03.04_all.deb \
+    keyring.deb SHA256:f9bb4340b5ce0ded29b7e014ee9ce788006e9bbfe31e96c09b2118ab91fca734
+sudo apt install ./keyring.deb
+echo "deb http://debian.sur5r.net/i3/ $(grep '^DISTRIB_CODENAME=' /etc/lsb-release | cut -f2 -d=) \
+    universe" | sudo tee /etc/apt/sources.list.d/sur5r-i3.list
+sudo apt update
+sudo apt install i3
+
+sudo apt -y install xorg lightdm lightdm-gtk-greeter i3status i3blocks dmenu \
     terminator tmux feh materia-gtk-theme papirus-icon-theme lxappearance fonts-font-awesome \
     picom fonts-droid-fallback
 sudo systemctl enable lightdm.service
+# i3lock-color
+sudo apt install autoconf gcc make pkg-config libpam0g-dev libcairo2-dev libfontconfig1-dev \
+    libxcb-composite0-dev libev-dev libx11-xcb-dev libxcb-xkb-dev libxcb-xinerama0-dev \
+    libxcb-randr0-dev libxcb-image0-dev libxcb-util-dev libxcb-xrm-dev libxkbcommon-dev \
+    libxkbcommon-x11-dev libjpeg-dev
+git clone https://github.com/Raymo111/i3lock-color.git
+cd i3lock-color
+./build.sh
+./install-i3lock-color.sh
 
 # zsh
 
